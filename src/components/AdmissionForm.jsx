73d7df57d8475f056ccab6b4e539d29a34a9146f
kavitasingh06 <kvsingh2321@gmail.com>
2026-05-25 15:18:51 +0530

    Use direct iframe for NoPaperForms widget

--- src/components/AdmissionForm.jsx
+++ src/components/AdmissionForm.jsx
@@ -2,17 +2,6 @@ import React, { useEffect } from 'react';
 import './AdmissionForm.css';
 
 const AdmissionForm = () => {
-  useEffect(() => {
-    // Only inject if it doesn't already exist to avoid StrictMode double-loading issues
-    if (!document.getElementById("npf-widget-script")) {
-      const script = document.createElement("script");
-      script.id = "npf-widget-script";
-      script.type = "text/javascript";
-      script.async = true;
-      script.src = "https://widgets.in6.nopaperforms.com/emwgts.js";
-      document.body.appendChild(script);
-    }
-  }, []);
 
   return (
     <div className="form-container-wrapper fade-in-up delay-200" id="admissions">
@@ -25,7 +14,14 @@ const AdmissionForm = () => {
           <h3 className="form-title">APPLY FOR SGVUEE 2026</h3>
         </div>
         
-        <div className="npf_wgts" data-height="400px" data-w="c2ccf50794637e9f986080d417cdaac8"></div>
+        <iframe 
+          src="https://widgets.in6.nopaperforms.com/register?&w=c2ccf50794637e9f986080d417cdaac8"
+          width="100%"
+          height="400px"
+          frameBorder="0"
+          title="Admission Form"
+          style={{ border: 'none', background: 'transparent', overflow: 'hidden' }}
+        ></iframe>
       </div>
     </div>
   );
